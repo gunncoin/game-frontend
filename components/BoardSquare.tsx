92c736b092d248e7c689c7b1ae12238451369abe
Ash Ranjan <ranjanashvin@gmail.com>
2022-02-06 11:51:01 -0800

    make things square

--- components/BoardSquare.tsx
+++ components/BoardSquare.tsx
@@ -18,13 +18,14 @@ const BoardSquare = (props: BoardSquareProps) => {
         textAlign: 'center',
         height: '4rem',
         backgroundColor: owner?.color,
-        borderRadius: '1rem',
+        //borderRadius: '1rem',
         borderStyle: 'solid',
         borderColor: selected ? 'gold' : 'white',
         borderWidth: '2px',
         color: 'white',
         fontSize: '25px',
         lineHeight: '3.4rem',
+        userSelect: 'none',
       }}
       onClick={onClick(position, owner)}
     >
